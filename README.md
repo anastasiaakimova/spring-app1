23c254ec6a63aa9b24d34cd538b37d93adf1bc1a
Anastasiya Akimova <anastasiyaaki171@gmail.com>
2021-02-06 16:53:12 +0300

    first example using spring

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Application created with Maven. 
+2. Using Spring. 
+3. Dependencies created with xml.
---
+++ src/main/java/org/example/TestBean.java
@@ -0,0 +1,20 @@
+package org.example;
+
+/**
+ * @author Anastaia Akimova
+ */
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
